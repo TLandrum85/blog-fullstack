bac7a38c707fa161168f37a0c6c6253b0639b2ec
Tracey <TlandrumRE@icloud.com>
2024-10-07 13:11:59 -0400

    Tailwind & Daisy setup

---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.querySelector('#app').innerHTML = `
+  <div class="p-4">
+    <h1 class="text-5xl mb-4">Welcome to LangTrack</h1>
+    <button class="btn btn-primary">Start Now</button>
+  </div>`
