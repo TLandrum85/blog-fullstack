99c35dc34742959cd5f87648afc3f5f1072a0703
Tracey <TlandrumRE@icloud.com>
2024-10-07 19:23:54 -0400

    Adds original code back in & APIs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import'./styles.css'
-document.querySelector('#app').innerHTML = `
-  <div class="p-4">
-    <h1 class="text-5xl mb-4">Welcome to Our Blog</h1>
-    <button class="btn btn-primary">Start Now</button>
-  </div>`
+const postsEl = document.querySelector('#posts');
+const API_URL = "https://bijjidesjxhsywmdfczi.supabase.co"
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpamppZGVzanhoc3l3bWRmY3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3NzIyNTYsImV4cCI6MjA0MjM0ODI1Nn0.JsxjgpNBhh-OndUDWIP-K1TUpYbtBClrK_tsUk9nXVA"
+
+
+fetch(`${API_URL}/blog_posts?select=*&apikey=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => showPostsWebView(item))
+    })
+
+
+function showPostsWebView(item = {}) {
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+            <div class="card bg-base-100 shadow-xl">
+<div class="card-body">
+                        <h3 class="card-title">${item['title']}</h3>
+                        <p class="text-gray-600">Published on: ${item['publishedOn']} by ${item['publishedBy']}</p>
+                        <p>${item['content']}</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+                </div>
+    `
+    postsEl.appendChild(div)
+}
+
