4deb38d1e8b90f37a1b4a31f4c833b3f23f0203c
Tracey <TlandrumRE@icloud.com>
2024-10-07 14:00:31 -0400

    fixes tailwind build

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+import'./styles.css'
 document.querySelector('#app').innerHTML = `
   <div class="p-4">
-    <h1 class="text-5xl mb-4">Welcome to LangTrack</h1>
+    <h1 class="text-5xl mb-4">Welcome to Our Blog</h1>
     <button class="btn btn-primary">Start Now</button>
   </div>`
